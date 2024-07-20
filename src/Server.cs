26969fd90b746f252f2e69085888ce11dc55055a
ildeberto <ildeberto34@gmail.com>
2024-07-20 23:48:50 +0100

    pass 1st stage

--- src/Server.cs
+++ src/Server.cs
@@ -3,3 +3,7 @@ using System.Net.Sockets;
 
 
 Console.WriteLine("Logs from your program will appear here!");
+
+ TcpListener server = new TcpListener(IPAddress.Any, 6379);
+ server.Start();
+ server.AcceptSocket(); // wait for client
