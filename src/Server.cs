d58a5cf99ba40a9871e8e32c07a33a27c05c0da1
ildeberto <ildeberto34@gmail.com>
2024-07-21 00:19:58 +0100

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -9,4 +9,4 @@ Console.WriteLine("Logs from your program will appear here!");
  server.AcceptSocket(); // wait for client
 
 var clientSocket=server.AcceptSocket();
-clientSocket.Send(Encoding.UTF8.GetBytes("+PONG\r\n"),SocketFlags.None);
+await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n" ),SocketFlags.None);
