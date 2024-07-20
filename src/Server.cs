0bb7073df699bae6bb814051b21a115e169410b9
ildeberto <ildeberto34@gmail.com>
2024-07-21 00:25:03 +0100

    Listen to ping

--- src/Server.cs
+++ src/Server.cs
@@ -2,10 +2,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-Console.WriteLine("Logs from your program will appear here!");
 
- TcpListener server = new TcpListener(IPAddress.Any, 6379);
- server.Start();
+TcpListener server = new TcpListener(IPAddress.Any, 6379);
+server.Start();
 Console.WriteLine(server.LocalEndpoint.Serialize());
 var clientSocket=server.AcceptSocket();
 await clientSocket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n" ),SocketFlags.None);
